c5b3f7faef1fc46ceee28bfb4912249f1d9b4210
Murad Cholukov <muradusa@gmail.com>
2020-10-05 07:18:17 -0700

    making Header responsive

--- src/Header.js
+++ src/Header.js
@@ -8,12 +8,13 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import logo from "./images/logo.png";
 import { Link } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__top">
-        <div className="header__topLeft">
+      <Grid container className="header__top">
+        <Grid item className="header__topLeft" xs={12} sm={6}>
           <div className="header__topLeftInfo">
             <PhoneEnabledIcon />
             <p>925-658-1600</p>
@@ -26,14 +27,40 @@ function Header() {
             <LocationOnIcon />
             <p>Walnut Creek, CA</p>
           </div>
-        </div>
-        <div className="header__topRight">
+        </Grid>
+        <Grid item className="header__topRight" xs={12} sm={6}>
           <FacebookIcon />
           <LinkedInIcon />
           <InstagramIcon />
-        </div>
-      </div>
-      <div className="header__bottom">
+        </Grid>
+      </Grid>
+
+      <Grid container className="header__bottom">
+        <Grid item className="header__bottomLeft" xs={12} sm={6}>
+          <img src={logo} alt="" />
+        </Grid>
+        <Grid item className="header__topRight" xs={12} sm={6}>
+          <Link className="header__link" to="/">
+            <h3>Home</h3>
+          </Link>
+          <Link className="header__link" to="/">
+            <h3>Services</h3>
+          </Link>
+
+          <Link className="header__link" to="/portfolio">
+            <h3>Portfolio</h3>
+          </Link>
+          <Link className="header__link" to="/about">
+            <h3>About</h3>
+          </Link>
+
+          <Link className="header__link" to="/contact">
+            <h3>Contact Us</h3>{" "}
+          </Link>
+        </Grid>
+      </Grid>
+
+      {/* <div className="header__bottom">
         <div className="header__bottomLeft">
           <img src={logo} alt="" />
         </div>
@@ -53,7 +80,7 @@ function Header() {
             <h3>Contact Us</h3>{" "}
           </Link>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
