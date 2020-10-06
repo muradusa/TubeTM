64cb91561683f670b966188458b0e86ca0999d91
Murad Cholukov <muradusa@gmail.com>
2020-10-06 14:14:17 -0700

    making Header responsive

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,8 +1,7 @@
-import { Grid } from "@material-ui/core";
 import React from "react";
 import "./Contact.css";
 
-function Contact() {
+function Contact1() {
   return (
     <div className="contact">
       <div className="contact__banner">
@@ -15,56 +14,48 @@ function Contact() {
         <p>Contact Us</p>
       </div>
       <div className="contact__section">
-        <Grid className="grid__container" container spacing={2}>
-          <Grid className="grid__containerItem" item xs={12} sm={12} md={6}>
-            <div className="contact__map">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.9024105233493!2d-122.05073624917259!3d37.90934367963577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808561b0b47cb7d7%3A0x1e96681da067a06!2s1301%20Ygnacio%20Valley%20Rd%2C%20Walnut%20Creek%2C%20CA%2094598!5e0!3m2!1sen!2sus!4v1601475404561!5m2!1sen!2sus"
-                width="100%"
-                height="100%"
-                frameBorder="0"
-                allowFullScreen="true"
-              ></iframe>
-            </div>
-          </Grid>
-          <Grid className="grid__containerItem" item xs={12} sm={12} md={6}>
-            <div className="contact__form">
-              <form name="contact" action="/contact" method="post">
-                <input type="hidden" name="form-name" value="contact" />
-                <p>Name</p>
-                <input
-                  required
-                  type="text"
-                  name="name"
-                  placeholder="Your Name"
-                />
-                <p>Email</p>
-                <input
-                  required
-                  type="email"
-                  name="email"
-                  placeholder="Your Email"
-                />
-                <p>Subject</p>
-                <input name="subject" type="text" placeholder="Subject" />
-                <p>Message</p>
-                <textarea
-                  required
-                  name="message"
-                  placeholder="Message"
-                  cols="30"
-                  rows="10"
-                ></textarea>
-                <button type="submit" className="contact__button">
-                  Send
-                </button>
-              </form>
-            </div>
-          </Grid>
-        </Grid>
+        <div className="contact__map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.9024105233493!2d-122.05073624917259!3d37.90934367963577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808561b0b47cb7d7%3A0x1e96681da067a06!2s1301%20Ygnacio%20Valley%20Rd%2C%20Walnut%20Creek%2C%20CA%2094598!5e0!3m2!1sen!2sus!4v1601475404561!5m2!1sen!2sus"
+            title="map"
+            alt="map"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allowFullScreen="true"
+          ></iframe>
+        </div>
+
+        <div className="contact__form">
+          <form name="contact" action="/contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <p>Name</p>
+            <input required type="text" name="name" placeholder="Your Name" />
+            <p>Email</p>
+            <input
+              required
+              type="email"
+              name="email"
+              placeholder="Your Email"
+            />
+            <p>Subject</p>
+            <input name="subject" type="text" placeholder="Subject" />
+            <p>Message</p>
+            <textarea
+              required
+              name="message"
+              placeholder="Message"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button type="submit" className="contact__button">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Contact;
+export default Contact1;
